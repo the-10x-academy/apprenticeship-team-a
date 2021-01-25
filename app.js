94311a861e40272c7a28c3ad5d78f514a4226abf
Anshika <anshika.cse20@gmail.com>
2021-01-25 19:53:22 +0530

    DB config

--- app.js
+++ app.js
@@ -3,11 +3,20 @@ const express = require("express");
 const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
+const mongoose = require("mongoose");
 
 const indexRouter = require("./routes/index");
 
 const app = express();
 
+//dbConfig
+mongoose
+	.connect("mongodb://localhost:27017/InstagramClone", {
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+	})
+	.then(() => console.log("connection Successful"))
+	.catch((err) => console.log(err));
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
