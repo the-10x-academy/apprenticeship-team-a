133954750928efc6a3476770d082ec63481434b3
akhilalapati <77323260+akhilalapati@users.noreply.github.com>
2021-01-25 18:37:56 +0530

    Landing Page Added

--- client/src/Components/HomePage/HomePage.js
+++ client/src/Components/HomePage/HomePage.js
@@ -1,18 +1,22 @@
-import logo from "../../logo.svg";
+//import logo from "../../logo.svg";
+import React from "react";
+import image from "../../lens-1418954@2x.png";
 import "./HomePage.css";
 
 function HomePage() {
-	return (
-		<div className="App">
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<p>
-					Welcome To 10xAcademy React BoilerPlate (created on top of FB
-					create-react-app)
-				</p>
-			</header>
-		</div>
+    return (
+
+      <div>
+        <div className="left">  {/* takes care of left arrangement */}
+            <img src={image} alt ="" className = 'img'></img>   {/* takes care of Image arrangement */}
+        </div>
+        <div className="right"> {/* takes care of right arrangement */}
+            <h1>10x Team 01</h1>
+            <button>Enter</button> {/* takes care of button */}
+        </div>
+      </div>
 	);
+	
 }
 
 export default HomePage;
