4afa963996e8bd635059533c2574fb71af58024c
akhilalapati <77323260+akhilalapati@users.noreply.github.com>
2021-01-26 11:29:48 +0530

    added landing page with image in public

--- client/src/Components/HomePage/HomePage.js
+++ client/src/Components/HomePage/HomePage.js
@@ -1,6 +1,5 @@
 //import logo from "../../logo.svg";
 import React from "react";
-import image from "../../lens-1418954@2x.png";
 import "./HomePage.css";
 
 function HomePage() {
@@ -8,7 +7,7 @@ function HomePage() {
 
       <div>
         <div className="left">  {/* takes care of left arrangement */}
-            <img src={image} alt ="" className = 'img'></img>   {/* takes care of Image arrangement */}
+            <img src= "/lens-1418954@2x.png" alt ="" className = 'img'></img>   {/* takes care of Image arrangement */}
         </div>
         <div className="right"> {/* takes care of right arrangement */}
             <h1>10x Team 01</h1>
@@ -18,5 +17,4 @@ function HomePage() {
 	);
 	
 }
-
 export default HomePage;
