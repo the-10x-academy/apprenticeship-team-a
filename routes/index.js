a6e465002cd7a8a87f33f15540b2e35540bf0649
akhilalapati <77323260+akhilalapati@users.noreply.github.com>
2021-01-26 09:33:29 +0530

    routes are added

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,21 @@ const express = require("express");
 const router = express.Router();
 
 /* GET home page. */
-router.get("/", function (req, res, next) {
-	res.render("index", { title: "Express" });
+router.get("/", function (req, res, next) {            /* Landing or HomePage */
+
 });
+
+router.get("/posts", function(req, res, next){           /* display the lists of post in db */
+
+});
+
+router.post("/createposts", function (req, res) {       /* fill the posts requirements and stores in db */	
+
+});
+
+router.get("/createposts", function(req, res, next){        /* display the Createposts page */
+
+});
+
 
 module.exports = router;
