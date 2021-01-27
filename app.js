498add4c7e05d36dcecf183a0e42a5f546ceebaf
Vineeth <malleshwaramvineeth@gmail.com>
2021-01-27 19:09:17 +0530

    Fetch and crete post

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
 const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
 
 const indexRouter = require("./routes/index");
 
@@ -23,6 +24,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use("/", indexRouter);
 
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,42 @@
 const express = require("express");
 const router = express.Router();
+const Post=require("../models/post");
+
+   
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
 	res.render("index", { title: "Express" });
 });
+
+/* create post */
+router.post("/createposts", function (req, res) {
+	var got = {
+		given_name : req.body.username,
+		given_location : req.body.location,
+	};
+	/*console.log(got,req.body)*/
+	var Details = new Post({
+		username : got.given_name,
+		location: got.given_location,
+	});
+	Details.save();
+	res.redirect("/dummyposts")
+});
+
+/* getiing post from db */
+router.get("/dummyposts", function(req, res){
+	Post.find()
+	.then(result=>{
+		res.status(200).json({
+			postData:result
+		});
+		console.log(result);
+	})
+	.catch(err=>{
+		console.log(err);
+		res.status(500).json({error:err});
+	})
+})
 
 module.exports = router;
