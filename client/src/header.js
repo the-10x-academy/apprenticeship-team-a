41f98a97696b0baa9bf238c37b26c49476ddf64d
Vineeth <malleshwaramvineeth@gmail.com>
2021-01-30 18:04:10 +0530

    added header

---
+++ client/src/header.js
@@ -0,0 +1,18 @@
+import React from "react";
+import icon from "./icon.png";
+import camera from "./camera.png";
+import "./header.css";
+
+function Header() {
+	return (
+		<div id="head">
+			<div id="insta">
+				<img src={icon} id="icon" alt="icon"></img>
+				<text id="logo">Instaclone</text>
+			</div>
+			<img src={camera} id="camera" alt="camera"></img>
+		</div>
+	);
+}
+
+export default Header;
