66484271fd467d3c8890701491316e3152ce3c1d
Anshika Singh <75901308+AnshikaCodeChef@users.noreply.github.com>
2021-02-01 20:07:43 +0530

    Combined components (#11)
    * added header in component folder
    
    * routing and compiled
    
    * modified
    
    * routes in smallletter
    
    * route changed for button
    
    * modified in Header and Button styling
    
    Co-authored-by: Anshika <anshika.cse20@gmail.com>

--- client/src/Components/InstagramPage/InstagramPage.js
+++ client/src/Components/InstagramPage/InstagramPage.js
@@ -1,20 +1,44 @@
 import React, { useState } from "react";
 import "./InstagramPage.css";
 import Post from "../Post/Post";
-​
+import Header from "../Header/Header";
 function InstagramPage() {
 	const [posts, setPosts] = useState([
 		{
-            username: "srikar",
+			username: "srikar",
 			location: "Hyderabad",
 			date: Date.now(),
-			description: "Iam srikar"
-			imageUrl: "https://www.w3schools.com/howto/img_avatar.png"
-        }
+			description: "Iam srikar",
+			imageUrl: "https://www.w3schools.com/howto/img_avatar.png",
+		},
+		{
+			username: "vineeeth",
+			location: "Bangalore",
+			date: Date.now(),
+			description: "OHH I AM MAKING INSTACLONE",
+			imageUrl:
+				"https://media.istockphoto.com/photos/child-hands-formig-heart-shape-picture-id951945718?k=6&m=951945718&s=612x612&w=0&h=ih-N7RytxrTfhDyvyTQCA5q5xKoJToKSYgdsJ_mHrv0=",
+		},
+		{
+			username: "anshika",
+			location: "Hyderabad",
+			date: Date.now(),
+			description: "AngelHere",
+			imageUrl:
+				"https://www.adobe.com/content/dam/cc/us/en/products/creativecloud/stock/stock-riverflow1-720x522.jpg.img.jpg",
+		},
+		{
+			username: "akhil",
+			location: "Hyderabad",
+			date: Date.now(),
+			description: "Solitude",
+			imageUrl:
+				"https://www.gettyimages.com/gi-resources/images/500px/983794168.jpg",
+		},
 	]);
-​
 	return (
 		<div>
+			<Header />
 			<div className="InstaPagePosts">
 				{posts.map((post) => (
 					<Post
@@ -30,5 +54,4 @@ function InstagramPage() {
 		</div>
 	);
 }
-​
 export default InstagramPage;
