f79968eef8785d490f11f6c8c454c0116dfef360
Vineeth <malleshwaramvineeth@gmail.com>
2021-02-02 22:24:41 +0530

    fetching data to FE

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const cookieParser = require("cookie-parser");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
+const cors = require("cors");
 
 const indexRouter = require("./routes/index");
 
@@ -29,6 +30,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
 
 app.use("/", indexRouter);
 
